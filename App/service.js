534fe5455cc492a443bc9b7213fac68bb9936dfc
Stranger123kr <nitesh8825kr@gmail.com>
2024-10-02 16:12:48 +0530

    some content changes

--- App/service.js
+++ App/service.js
@@ -6,48 +6,22 @@ const role = document.querySelector(".role");
 
 const testimonials = [
   {
-    name: "Miyah Myles",
-    position: "Marketing",
-    photo:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6",
-    text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+    name: "Priya",
+    position: "Delhi, India",
+    photo: "https://randomuser.me/api/portraits/women/15.jpg",
+    text: "I had an exceptional experience with LDD Realty! Their team understood my needs and guided me through every step. I am now a proud homeowner, thanks to their expertise and dedication!",
   },
   {
-    name: "June Cha",
-    position: "Software Engineer",
-    photo: "https://randomuser.me/api/portraits/women/44.jpg",
-    text: "This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!",
+    name: "Amit Singh",
+    position: "Dubai",
+    photo: "https://images.unsplash.com/photo-1628264047194-fbef620a1262?w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fGluZGlhbiUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+    text: "LDD Realty made my property search hassle-free. Their professional team provided great insights and helped me find the perfect apartment. Highly recommend their services!",
   },
   {
-    name: "Iida Niskanen",
-    position: "Data Entry",
-    photo: "https://randomuser.me/api/portraits/women/68.jpg",
-    text: "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
-  },
-  {
-    name: "Renee Sims",
-    position: "Receptionist",
-    photo: "https://randomuser.me/api/portraits/women/65.jpg",
-    text: "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
-  },
-  {
-    name: "Jonathan Nunfiez",
-    position: "Graphic Designer",
-    photo: "https://randomuser.me/api/portraits/men/43.jpg",
-    text: "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
-  },
-  {
-    name: "Sasha Ho",
-    position: "Accountant",
-    photo:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb",
-    text: "This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!",
-  },
-  {
-    name: "Veeti Seppanen",
-    position: "Director",
-    photo: "https://randomuser.me/api/portraits/men/97.jpg",
-    text: "This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.",
+    name: " Neha Gupta",
+    position: "Goa, India",
+    photo: "https://plus.unsplash.com/premium_photo-1682092039530-584ae1d9da7f?w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGluZGlhbiUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+    text: "I am incredibly grateful to LDD Realty for their assistance in buying my first home. Their knowledge of the market and commitment to client satisfaction is truly commendable!",
   },
 ];
 
